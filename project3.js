74f4c3e169c3a9bf1fd4c65f64dfc4691d2315c2
ivazquez1 <ivazquez1@fullsail.edu>
2015-01-22 22:07:14 -0600

    commit
    adding flowchart (3)

--- project3.js
+++ project3.js
@@ -8,9 +8,11 @@
 var doggySpas = ["Dog Spa bath", "Dog Spa hair trim", "Dog Spa nail trim"];
 var minutesPerSpa = [30, 45, 25];
 var dogNames = ["Odie","Bentley","Kita"];
-    
-// my functions 
+var paymentCreditCard = "master card";
 
+    
+ 
+// Array
  console.log(dogNames);
 	
 	dogNames[0] = "Odie";
@@ -20,14 +22,8 @@ var dogNames = ["Odie","Bentley","Kita"];
 	console.log("How many dogs do I have " + dogNames.length);
 	console.log("Only dropped off one of my dogs at the " + doggySpas + ".");
 	
-	
-  var visitDogSpa = function(theSpa) {
 
-    var doggySpa = doggySpas[theSpa],
-    
-        minutesThisSpa = minutesPerSpa[spaNum];
-        
-// Conditionals
+// Argument: Boolean
 var dogSpaOpen = true;
 
 if (dogSpaOpen === true) {
@@ -36,10 +32,28 @@ if (dogSpaOpen === true) {
 	console.log("Odie will be dirty and smelly!");
 	            
 }; 
-	       
+
+// Return Boolean
+var paymentCreditCard = true;
+
+if (paymentCreditCard === true) {
+	console.log("Yes, charge it on my master card.");
+} else {
+	console.log("No, I don't have cash I will have to stop by atm.");
+	
+};		       
+
+	
+// function	
+  var visitDogSpa = function(theSpa) {
+
+    var doggySpa = doggySpas[theSpa],
+    
+        minutesThisSpa = minutesPerSpa[spaNum];
+ 
+        
              
-      
-// While Loop        
+// for Loop        
     console.log(dogNames[0] + " went to the " + doggySpa +
         " for " + minutesThisSpa + " minutes. So I set the time on" +
         " my iphone.");
@@ -52,21 +66,26 @@ if (dogSpaOpen === true) {
                 minutes + " minutes have past.");
                 
         };
-    console.log("We finished with the " + doggySpa + ".");
-    console.log("Odie is ready for pick up.");
+        
+         console.log("We finished with the " + doggySpa + "."); 
     
-
 };
+
+  
 for (var spaNum = 0; spaNum < doggySpas.length; spaNum++) {
     visitDogSpa(spaNum);
     };
-
+    
+    console.log("Odie is ready for pick up.");
+        
+    
+     
 // JSON data
 var myJSON = function() {
 
 	console.log(jsonData);
     
-
      
-     };   
+};   
+     
    
